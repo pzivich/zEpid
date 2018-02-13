5e61c7dab88349b44da191f604afb349bfffce5b
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2018-02-13 12:02:19 -0500

    Update setup.py
    Updating dependencies

--- zepid/setup.py
+++ zepid/setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup 
 
-setup(name='zepid',version='0.1.0',description='Basic tool package for epidemiologic analyses',
-      long_description='This package contains some essential epidemiological tools for epidemiology analyses in Python 3.x. Available'
-      ' tools include summary measures (RR, OR, RD, NNT, IRD, IRR, PAF, ACR), these same measures for pandas '
-      'dataframes, IC and ICR, effect measure plots, senstivity analysis tools, and inverse probability weights.',
-      author='Paul Zivich',author_email='zivich.5@gmail.com',install_requires=['pandas',
-      'numpy','statsmodels','matplotlib','scipy'])
+setup(name='zepid',version='0.1.0',description='Tool package for epidemiologic analyses',
+      long_description='''This package contains some essential epidemiological tools for epidemiology analyses in Python 3.5+ Available 
+      tools include basic association meaures, IC and ICR, effect measure plots, functional form assessments,
+      senstivity analysis tools, and inverse probability weights.''',
+      author='Paul Zivich',author_email='zepidpy@gmail.com',install_requires=['pandas',
+      'numpy','statsmodels','matplotlib','scipy','networkx','lifelines','tabulate'])
