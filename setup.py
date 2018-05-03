0a3d1d181a18a94214afb96a0e947c126af40f97
pzivich <pzivich@live.unc.edu>
2018-05-03 17:14:29 -0400

    setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup 
 
-setup(
-    name='zepid',
+
+setup(name='zepid',
     version='0.1.0',
     description='Tool package for epidemiologic analyses',
     author='Paul Zivich',
