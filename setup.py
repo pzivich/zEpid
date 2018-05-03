d1c36fc7c7beffbfd327e90d56b754e01d23a94b
Paul Zivich <32672909+pzivich@users.noreply.github.com>
2018-05-03 07:15:57 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='zepid',
-    version='0.1.0',
+    version='0.1.1',
     description='Tool package for epidemiologic analyses',
     author='Paul Zivich',
     author_email='zepidpy@gmail.com',
     url = 'https://github.com/pzivich/zepid',
     classifiers = ['Programming Language :: Python :: 3.5']
-    install_requires=['pandas'>=0.18,'numpy','statsmodels','matplotlib','scipy','networkx','lifelines','tabulate'],
-    keywords=['epidemiology'])
+    install_requires=['pandas'>=0.18,'numpy','statsmodels','matplotlib','scipy','lifelines','tabulate'],
+    keywords=['epidemiology','inverse-probability-weights'])
