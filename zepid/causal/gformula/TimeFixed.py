74093452cfd1056ec97a5425c7b10e6d05a78470
pzivich <pzivich@live.unc.edu>
2018-07-26 12:22:19 -0400

    Fixed missing data part. Changed to follow PEP guidelines better

--- zepid/causal/gformula/TimeFixed.py
+++ zepid/causal/gformula/TimeFixed.py
@@ -1,9 +1,10 @@
-import numpy as np 
+import warnings
+import numpy as np
 import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
-from statsmodels.genmod.families import family
 from statsmodels.genmod.families import links
+
 
 class TimeFixedGFormula:
     '''Time-fixed implementation of the g-formula, also referred to as the g-computation
@@ -40,17 +41,18 @@ class TimeFixedGFormula:
     outcome_type:
         -outcome variable type. Currently only 'binary' or 'continuous' variable types are supported
     '''
-    def __init__(self,df,exposure,outcome,outcome_type='binary'):
+    def __init__(self, df, exposure, outcome, outcome_type='binary'):
         self.gf = df.copy()
         self.exposure = exposure
         self.outcome = outcome
-        if  (outcome_type == 'binary') or (outcome_type == 'continuous'):
+        if (outcome_type == 'binary') or (outcome_type == 'continuous'):
             self.outcome_type = outcome_type
         else:
-            raise ValueError('Only binary or continuous outcomes are currently supported. Please specify "binary" or "continuous"')
+            raise ValueError('Only binary or continuous outcomes are currently supported. Please specify "binary" or '
+                             '"continuous"')
         self.model_fit = False
     
-    def outcome_model(self,model,print_model_results=True):
+    def outcome_model(self, model, print_model_results=True):
         '''Build the model for the outcome. This is also referred to at the Q-model. This must be specified
         before the fit function. If it is not, an error will be raised.
         
@@ -64,23 +66,24 @@ class TimeFixedGFormula:
             -whether to print the logistic regression results to the terminal. Default is True
         '''
         if self.outcome_type == 'binary':
-            linkdist=sm.families.family.Binomial(sm.families.links.logit)
+            linkdist = sm.families.family.Binomial(sm.families.links.logit)
         else:
-            linkdist=sm.families.family.Gaussian(sm.families.links.identity)
+            linkdist = sm.families.family.Gaussian(sm.families.links.identity)
         
-        #Modeling the outcome
-        m = smf.glm(self.outcome+' ~ '+model,self.gf,family=linkdist)
+        # Modeling the outcome
+        m = smf.glm(self.outcome+' ~ '+model, self.gf, family=linkdist)
         self.outcome_model = m.fit()
-        if print_model_results == True:
+        if print_model_results is True:
+            # Warning to make users aware that some amount of observations are dropped
+            if self.gf.shape[0] != self.outcome_model.nobs:
+                warnings.warn('Notice: ' +
+                              str(self.gf.shape[0] - self.outcome_model.nobs) +
+                              ' observations were dropped due to missing data for at least one of the variables in the '
+                              'specified regression model')
             print(self.outcome_model.summary())
         self.model_fit = True
-        
-        #Warning to make users aware that some amount of observations are dropped
-        if self.gf.shape[0] != self.outcome_model.nobs:
-            warnings.warn('Notice: ',self.gf.shape[0] - self.outcome_model.nobs,' observations were dropped due' 
-            ' to missing data for at least one of the variables in the specified regression model')
-    
-    def fit(self,treatment):
+
+    def fit(self, treatment):
         '''Fit the parametric g-formula as specified. Binary and multivariate treatments are available.
         This implementation has three options for the binary treatment courses:
     
@@ -100,40 +103,41 @@ class TimeFixedGFormula:
             -specified treatment course. Either a string object for binary treatments or a list of custom
              treatments as strings
         '''
-        if self.model_fit == False:
+        if self.model_fit is False:
             raise ValueError('Before the g-formula can be calculated, the outcome model must be specified')
         if (type(treatment) != str) and (type(treatment) != list):
             raise ValueError('Specified treatment must be a string object or a list of string objects')
         
-        #Setting outcome as blank
+        # Setting outcome as blank
         g = self.gf.copy()
         
-        #Setting treatment (either multivariate or binary)
-        if type(self.exposure) == list: #Multivariate exposure
-            if (treatment == 'all') or (treatment == 'none'): #Check to make sure custom treatment
+        # Setting treatment (either multivariate or binary)
+        if type(self.exposure) == list:  # Multivariate exposure
+            if (treatment == 'all') or (treatment == 'none'): # Check to make sure custom treatment
                 raise ValueError('A multivariate exposure has been specified. A custom treatment must be '
-                                'specified by the user')
+                                 'specified by the user')
             else:
-                if len(self.exposure) != len(treatment): #Check to make sure same about of treatments specified and exposures
+                if len(self.exposure) != len(treatment): # Check to make sure same about of treatments specified
                     raise ValueError('The list of custom treatment conditions must be the same size as the number of '
-                                    'treatments')
+                                     'treatments')
                 for i in range(len(self.exposure)):
                     g[self.exposure[i]] = np.where(eval(treatment[i]),1,0)
-                if np.sum(np.where(g[self.exposure].sum(axis=1)>1,1,0)) > 1: #warning is some individuals receive multiple treatments
-                    warnings.warn('It looks like your specified treatment strategy results in some individuals receiving'
-                                ' at least two exposures. Reconsider how the custom treatments are specified')
+                if np.sum(np.where(g[self.exposure].sum(axis=1)>1,1,0)) > 1:
+                    warnings.warn('It looks like your specified treatment strategy results in some individuals '
+                                  'receiving at least two exposures. Reconsider how the custom treatments are '
+                                  'specified')
         
-        else: #Binary exposure
+        else:  # Binary exposure
             if type(treatment) == list:
                 raise ValueError('A binary exposure is specified. Treatment plan should be a string object')
             if treatment == 'all':
                 g[self.exposure] = 1
             elif treatment == 'none':
                 g[self.exposure] = 0
-            else: #custom exposure pattern
+            else:  # custom exposure pattern
                 g[self.exposure] = np.where(eval(treatment),1,0)
         
-        #Getting predictions
+        # Getting predictions
         g[self.outcome] = np.nan
         g[self.outcome] = self.outcome_model.predict(g)
         self.marginal_outcome = np.mean(g[self.outcome])
