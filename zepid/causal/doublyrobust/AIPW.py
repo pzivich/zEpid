04a65810a8bd4b998ddd7dd7ba3ca7680e4d0795
pzivich <pzivich@live.unc.edu>
2018-11-25 14:54:44 -0500

    Update to AIPW docs

--- zepid/causal/doublyrobust/AIPW.py
+++ zepid/causal/doublyrobust/AIPW.py
@@ -6,21 +6,35 @@ from zepid.causal.ipw import propensity_score
 
 
 class AIPW:
-    """Implementation of augmented inverse probablity weight estimator as described in Funk et al. American Journal
-    of Epidemiology 2011;173(7):761-767. The exact formulas used are available in Table 1 and Equation 1. Properties
-    of the doubly robust estimator are often misunderstood, so we direct the reader to  Keil et al. AJE
-    2018;187(4):891-892. SimplyDoubleRobust only supports a binary exposure and binary outcome. Also this model does
-    not deal with missing data or time varying exposures
-
-    df:
-        -pandas DataFrame object containing all variables of interest
-    exposure:
-        -column name of the exposure variable. Currently only binary is supported
-    outcome:
-        -column name of the outcome variable. Currently only binary is supported
-    """
-
     def __init__(self, df, exposure, outcome):
+        """Implementation of augmented inverse probablity weight estimator. This is a simple implementation of AIPW
+        and does not account for missing data. It only handles time-fixed confounders and treatments at this point.
+
+        The augment-inverse probability weights are calculated from the following formula
+
+        .. math::
+
+            RD = (\frac{Y_{A=1}*A}{\Pr(A=1)} - \frac{\hat{Y}_1*(A-\Pr(A=1)}{\Pr(A=1)}) - (\frac{Y_{A=0}*A}{\Pr(A=0)
+            } - \frac{\hat{Y}_0*(A-\Pr(A=1)}{\Pr(A=0)})
+            RR = (\frac{Y_{A=1}*A}{\Pr(A=1)} - \frac{\hat{Y}_1*(A-\Pr(A=1)}{\Pr(A=1)}) / (\frac{Y_{A=0}*A}{\Pr(A=0)
+            } - \frac{\hat{Y}_0*(A-\Pr(A=1)}{\Pr(A=0)})
+
+        Confidence intervals should be calculated using non-parametric bootstrapping
+
+        Parameters
+        ----------
+        df : DataFrame
+            Pandas DataFrame object containing all variables of interest
+        exposure : str
+            Column name of the exposure variable. Currently only binary is supported
+        outcome : str
+            Column name of the outcome variable. Currently only binary is supported
+
+        References
+        ----------
+        Funk, M. J., Westreich, D., Wiesen, C., Stürmer, T., Brookhart, M. A., & Davidian, M. (2011). Doubly robust
+        estimation of causal effects. American journal of epidemiology, 173(7), 761-767.
+        """
         self.df = df.copy()
         self._exposure = exposure
         self._outcome = outcome
@@ -33,13 +47,15 @@ class AIPW:
         self._out_model = None
 
     def exposure_model(self, model, print_results=True):
-        """Used to specify the propensity score model. Model used to predict the exposure via a logistic regression
-        model
+        """Specify the propensity score / inverse probability weight model. Model used to predict the exposure via a
+        logistic regression model
 
-        model:
-            -Independent variables to predict the exposure. Example) 'var1 + var2 + var3'
-        print_results:
-            -Whether to print the fitted model results. Default is True (prints results)
+        Parameters
+        ----------
+        model : str
+            Independent variables to predict the exposure. For example, 'var1 + var2 + var3'
+        print_results : bool, optional
+            Whether to print the fitted model results. Default is True (prints results)
         """
         self._exp_model = self._exposure + ' ~ ' + model
         fitmodel = propensity_score(self.df, self._exp_model, print_results=print_results)
@@ -47,16 +63,17 @@ class AIPW:
         self._fit_exposure_model = True
 
     def outcome_model(self, model, print_results=True):
-        """Used to specify the outcome model. Model used to predict the outcome via a logistic regression model
+        """Specify the outcome model. Model used to predict the outcome via a logistic regression model
 
-        model:
-            -Independent variables to predict the outcome. Example) 'var1 + var2 + var3 + var4'
-        print_results:
-            -Whether to print the fitted model results. Default is True (prints results)
+        Parameters
+        ----------
+        model : str
+            Independent variables to predict the outcome. For example, 'var1 + var2 + var3 + var4'
+        print_results : bool, optional
+            Whether to print the fitted model results. Default is True (prints results)
         """
-
         self._out_model = self._outcome + ' ~ ' + model
-        f = sm.families.family.Binomial(sm.families.links.logit)
+        f = sm.families.family.Binomial(sm.families.links.logit)  # TODO add option for continuous Y?
         log = smf.glm(self._out_model, self.df, family=f).fit()
         if print_results:
             print('\n----------------------------------------------------------------')
@@ -74,20 +91,20 @@ class AIPW:
 
     def fit(self):
         """Once the exposure and outcome models are specified, we can estimate the Risk Ratio and Risk Difference.
-        This function generates the estimated risk difference and risk ratio.  To view results, use AIPW.summary() For
-        confidence intervals, a bootstrap procedure should be used
+        This function generates the estimated risk difference and risk ratio. For confidence intervals, a non-parametric
+        bootstrap procedure should be used
         """
         if (self._fit_exposure_model is False) or (self._fit_exposure_model is False):
             raise ValueError('The exposure and outcome models must be specified before the doubly robust estimate can '
                              'be generated')
 
-        # Doubly robust estimator for exposed
+        # Doubly robust estimator under all treated
         self.df['dr1'] = np.where(self.df[self._exposure] == 1,
                                   ((self.df[self._outcome]) / self.df['ps']) - (((self.df['pY1'] * (1 - self.df['ps']))
                                                                                  / (self.df['ps']))),
                                   self.df['pY1'])
 
-        # Doubly robust estimator for unexposed
+        # Doubly robust estimator under all untreated
         self.df['dr0'] = np.where(self.df[self._exposure] == 0,
                                   (self.df['pY0']),
                                   ((self.df[self._outcome]) / (1 - self.df['ps']) - (
@@ -101,8 +118,10 @@ class AIPW:
     def summary(self, decimal=4):
         """Prints a summary of the results for the doubly robust estimator.
 
-        decimal:
-            -number of decimal places to display in the result
+        Parameters
+        ----------
+        decimal : int, optional
+            Number of decimal places to display in the result
         """
         if (self._fit_exposure_model is False) or (self._fit_exposure_model is False):
             raise ValueError('The exposure and outcome models must be specified before the double robust estimate can '
