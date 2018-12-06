8f9c6366821f56f52633b0b0e9e3e85569470a0c
pzivich <pzivich@live.unc.edu>
2018-12-06 12:19:23 -0500

    Completed testing for loading data sets

---
+++ tests/test_datasets.py
@@ -0,0 +1,107 @@
+import pandas as pd
+
+import zepid as ze
+
+
+class TestSampleData:
+
+    def test_return_pandas(self):
+        df = ze.load_sample_data(False)
+        assert isinstance(df, type(pd.DataFrame()))
+
+    def test_correct_nobs_timefixed(self):
+        df = ze.load_sample_data(False)
+        assert df.shape[0] == 547
+
+    def test_correct_ncols_timefixed(self):
+        df = ze.load_sample_data(False)
+        assert df.shape[1] == 8
+
+    def test_missing_in_timefixed(self):
+        df = ze.load_sample_data(False)
+        assert df.shape[0] != df.dropna().shape[0]
+
+    def test_correct_nobs_timevary(self):
+        df = ze.load_sample_data(True)
+        assert df.shape[0] == 27382
+
+    def test_correct_ncols_timevary(self):
+        df = ze.load_sample_data(True)
+        assert df.shape[1] == 12
+
+
+class TestEwingSarcoma:
+
+    def test_return_pandas(self):
+        df = ze.load_ewing_sarcoma_data()
+        assert isinstance(df, type(pd.DataFrame()))
+
+    def test_correct_nobs(self):
+        df = ze.load_ewing_sarcoma_data()
+        assert df.shape[0] == 76
+
+    def test_correct_ncols(self):
+        df = ze.load_ewing_sarcoma_data()
+        assert df.shape[1] == 4
+
+    def test_no_missing(self):
+        df = ze.load_ewing_sarcoma_data()
+        assert df.shape[0] == df.dropna().shape[0]
+
+
+class TestGvHD:
+
+    def test_return_pandas(self):
+        df = ze.load_gvhd_data()
+        assert isinstance(df, type(pd.DataFrame()))
+
+    def test_correct_nobs(self):
+        df = ze.load_gvhd_data()
+        assert df.shape[0] == 108714
+
+    def test_correct_ncols(self):
+        df = ze.load_gvhd_data()
+        assert df.shape[1] == 30
+
+    def test_no_missing(self):
+        df = ze.load_gvhd_data()
+        assert df.shape[0] == df.dropna().shape[0]
+
+
+class TestSciatica:
+
+    def test_return_pandas(self):
+        df = ze.load_sciatica_data()
+        assert isinstance(df, type(pd.DataFrame()))
+
+    def test_correct_nobs(self):
+        df = ze.load_sciatica_data()
+        assert df.shape[0] == 1240
+
+    def test_correct_ncols(self):
+        df = ze.load_sciatica_data()
+        assert df.shape[1] == 17
+
+    def test_no_missing(self):
+        df = ze.load_sciatica_data()
+        assert df.shape[0] == df.dropna().shape[0]
+
+
+
+class TestLeukemia:
+
+    def test_return_pandas(self):
+        df = ze.load_leukemia_data()
+        assert isinstance(df, type(pd.DataFrame()))
+
+    def test_correct_nobs(self):
+        df = ze.load_leukemia_data()
+        assert df.shape[0] == 42
+
+    def test_correct_ncols(self):
+        df = ze.load_leukemia_data()
+        assert df.shape[1] == 5
+
+    def test_no_missing(self):
+        df = ze.load_leukemia_data()
+        assert df.shape[0] == df.dropna().shape[0]
