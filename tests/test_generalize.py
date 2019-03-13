3bb4e8c61af2320952aa0ea91f94c9cc298f58a3
pzivich <pzivich@live.unc.edu>
2019-03-13 17:29:39 -0400

    Adding g-transport tests

--- tests/test_generalize.py
+++ tests/test_generalize.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy.testing as npt
 
 import zepid as ze
-from zepid.causal.generalize import IPSW
+from zepid.causal.generalize import IPSW, GTransportFormula
 from zepid.causal.ipw import IPTW
 
 
@@ -30,7 +30,6 @@ class TestIPSW:
         ipt.fit()
         dfs['iptw'] = ipt.Weight
         return pd.concat([dfs, df_c.loc[df_c['S'] == 0]], ignore_index=True, sort=False)
-
 
     def test_stabilize_error(self, df_c):
         ipsw = IPSW(df_c, exposure='A', outcome='Y', selection='S', stabilized=False)
@@ -63,7 +62,6 @@ class TestIPSW:
         npt.assert_allclose(ipsw.risk_difference, 0.034896, atol=1e-5)
         npt.assert_allclose(ipsw.risk_ratio, 1.097139, atol=1e-4)
 
-
     def test_transport_stabilized(self, df_r):
         ipsw = IPSW(df_r, exposure='A', outcome='Y', selection='S', stabilized=True, generalize=False)
         ipsw.regression_models('L + W_sq', print_results=False)
@@ -84,3 +82,37 @@ class TestIPSW:
         ipsw.fit()
         npt.assert_allclose(ipsw.risk_difference, 0.047296, atol=1e-5)
         npt.assert_allclose(ipsw.risk_ratio, 1.1372, atol=1e-4)
+
+
+class TestGTransport:
+
+    @pytest.fixture
+    def df_r(self):
+        df = ze.load_generalize_data(False)
+        df['W_sq'] = df['W']**2
+        return df
+
+    @pytest.fixture
+    def df_c(self):
+        df = ze.load_generalize_data(True)
+        df['W_sq'] = df['W']**2
+        return df
+
+    def test_no_model_error(self, df_c):
+        gtf = GTransportFormula(df_c, exposure='A', outcome='Y', selection='S', generalize=True)
+        with pytest.raises(ValueError):
+            gtf.fit()
+
+    def test_generalize_stabilized(self, df_r):
+        gtf = GTransportFormula(df_r, exposure='A', outcome='Y', selection='S', generalize=True)
+        gtf.outcome_model('A + L + L:A + W_sq + W_sq:A + W_sq:A:L', print_results=False)
+        gtf.fit()
+        npt.assert_allclose(gtf.risk_difference, 0.064038, atol=1e-5)
+        npt.assert_allclose(gtf.risk_ratio, 1.203057, atol=1e-4)
+
+    def test_transport_unstabilized(self, df_r):
+        gtf = GTransportFormula(df_r, exposure='A', outcome='Y', selection='S', generalize=False)
+        gtf.outcome_model('A + L + L:A + W_sq + W_sq:A + W_sq:A:L', print_results=False)
+        gtf.fit()
+        npt.assert_allclose(gtf.risk_difference, 0.058573, atol=1e-5)
+        npt.assert_allclose(gtf.risk_ratio, 1.176615, atol=1e-4)
